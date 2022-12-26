824feffa97db09bf3c0b31e3c9595341c82f22a6
Bessawy <amrmostafa5557@gmail.com>
2022-12-26 15:50:51 -0800

    Added footer and modified Nav

--- src/Pages/routes.tsx
+++ src/Pages/routes.tsx
@@ -5,12 +5,16 @@ import { useAppDispatch, useAppSelector } from "../reduxhook/hooks";
 import { toggleThemeMode } from "../redux/reducers/themeModeReducer";
 import {
   AppBar,
+  Box,
+  Divider,
+  Grid,
+  Paper,
   Toolbar,
   Typography,
 } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { NavLink, Outlet } from "react-router-dom";
-
+import { Container } from "@mui/system";
 
 export const Routes = () => {
   const mode = useAppSelector((state) => state.themeReducer) as
@@ -34,26 +38,84 @@ export const Routes = () => {
             p: 0,
           }}
         >
+          <NavLink to="/">
+            {" "}
+            <Box
+              className="logo_img"
+              sx={{ width: 230, height: 50, marginRight: 10 }}
+            ></Box>
+          </NavLink>
           <Typography>
             <NavLink to="/"> Home </NavLink>
           </Typography>
-          <Typography sx={{ml: 1}}>
+          <Typography sx={{ ml: 1 }}>
             <NavLink to="/products"> Products </NavLink>
           </Typography>
-
           <IconButton sx={{ marginLeft: "auto" }} onClick={toggleTheme}>
             {mode === "dark" ? <ModeNightIcon /> : <LightModeIcon />}
           </IconButton>
-          <ShoppingCartIcon />
+          <IconButton>
+            <ShoppingCartIcon />
+          </IconButton>
+          <IconButton>
+            <Box className="login_img"
+              sx={{ width: 50, height: 50}}></Box>
+          </IconButton>
         </Toolbar>
       </AppBar>
 
-      <Outlet/>
+      <Outlet />
 
       <footer>
-
+        <Paper sx={{ marginTop: 10, paddingBottom: 10, paddingTop: 6 }}>
+          <Container maxWidth="lg">
+            <Typography variant="subtitle2">
+              <Grid container spacing={3}>
+                <Grid item xs={4}>
+                  <Box borderBottom={1} marginBottom={2}>
+                    {" "}
+                    Help
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Contact </NavLink>
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Support </NavLink>
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Privacy </NavLink>
+                  </Box>
+                </Grid>
+                <Grid item xs={4}>
+                  <Box borderBottom={1} marginBottom={2}>
+                    Account
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Login </NavLink>
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Register </NavLink>
+                  </Box>
+                </Grid>
+                <Grid item xs={4}>
+                  <Box borderBottom={1} marginBottom={2}>
+                    Navigate
+                  </Box>
+                  <Box>
+                    <NavLink to="/"> Home </NavLink>
+                  </Box>
+                  <Box>
+                    <NavLink to="/products"> Products </NavLink>
+                  </Box>
+                </Grid>
+              </Grid>
+              <Box textAlign="center" sx={{ margin: 4 }} fontWeight={400}>
+                <b>AMR</b> Shopping Center &copy; {new Date().getFullYear()}
+              </Box>
+            </Typography>
+          </Container>
+        </Paper>
       </footer>
     </div>
   );
 };
-
